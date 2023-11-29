1cc35e59a5996cee1ebd0cc764789fcc460b23bb
Arafat <mdarafathosainchowdhory@gmail.com>
2023-11-29 17:18:49 +0600

    Add items in set

---
+++ Add_Items_in_Set.py
@@ -0,0 +1,9 @@
+fruits = {"Banana", "Apple", "Mango", 200}
+
+print(fruits)
+
+fruits.add("Kiwi")
+fruits.add((1, 2)) #Tuple
+# fruits.add([1, 2]) #[1, 2] is a list, and that will not work because it is mutable
+
+print(fruits)
